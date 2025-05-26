820873ddc26512a06e46fb2de9460e0510932d6b
Osman Jime <ohjime@icloud.com>
2025-05-26 13:45:00 -0600

    Removed Different Font when in Dark mode

--- lib/core/style/themes.dart
+++ lib/core/style/themes.dart
@@ -10,24 +10,16 @@ ThemeData getCoreTheme({
     brightness: brightness,
   );
   // Return ThemeData with the colorScheme
-  return ThemeData(
-    colorScheme: colorScheme,
-    useMaterial3: true,
-    scaffoldBackgroundColor:
-        brightness == Brightness.light
-            ? colorScheme.surfaceContainerLowest
-            : colorScheme.surfaceContainerHighest,
-  );
+  return ThemeData(colorScheme: colorScheme, useMaterial3: true);
 }
 
 // Use the function to create the light theme
-final ThemeData lightTheme =
-    getCoreTheme(
-      seedColor: Colors.brown,
-      brightness: Brightness.light,
-    ).copyWith();
+final ThemeData lightTheme = getCoreTheme(
+  seedColor: Colors.blueGrey,
+  brightness: Brightness.light,
+).copyWith();
 // Use the function to create the dark theme
 final ThemeData darkTheme = getCoreTheme(
-  seedColor: Colors.green,
+  seedColor: Colors.amber,
   brightness: Brightness.dark,
-).copyWith(textTheme: ThemeData.dark().textTheme.apply(fontFamily: 'Poppins'));
+);
